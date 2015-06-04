3e1c5dff37da49c982b765c053869bd85f230ab9
hannah <hannah@hannah.com>
2015-06-04 15:28:45 -0700

    gene and ensembl id added to ProteinCurrent object

--- src/main/java/org/copakb/updater/protein/ProteinUpdate.java
+++ src/main/java/org/copakb/updater/protein/ProteinUpdate.java
@@ -9,14 +9,18 @@ import uk.ac.ebi.kraken.interfaces.uniprot.dbx.go.OntologyType;
 import uk.ac.ebi.kraken.interfaces.uniprot.features.Feature;
 import uk.ac.ebi.kraken.uuw.services.remoting.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.Connection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by vincekyi on 5/26/15.
@@ -28,7 +32,7 @@ public class ProteinUpdate {
     }
 
     public static void main(String[] args) {
-        update("/Users/vincekyi/IdeaProjects/DatabaseUpdater/src/main/resources/test.fasta");
+        update("./src/main/resources/test.fasta");
     }
 
 
@@ -52,6 +56,7 @@ public class ProteinUpdate {
                     uniprotid = uniprotheader.substring(4,uniprotheader.indexOf("|",4) ) ;
                     try{
                         entry = (UniProtEntry)entryRetrievalService.getUniProtEntry(uniprotid);
+                        System.out.println("\n*************************");
                         System.out.println("Able to retrieve "+uniprotid+" from UniProt");
 
                     }catch(Exception ex){
@@ -126,8 +131,6 @@ public class ProteinUpdate {
             return null;
         }
 
-
-
         // fill in transmembrane/cytoplasmic/noncytoplasmic domain, signal peptides, and features
         boolean foundCytoplasmic = true;
         String transmem = "", cytoplasmic = "", noncytoplasmic = "", signal = "", features = "";
@@ -156,16 +159,6 @@ public class ProteinUpdate {
         result.setSignal_peptide(ValideSQL(signal));
         result.setFeature_table(ValideSQL(features));
 
-        // get gene name
-        Set<org.copakb.server.dao.model.Gene> genes = new HashSet<org.copakb.server.dao.model.Gene>();
-        for(Gene gene: e.getGenes()) {
-            org.copakb.server.dao.model.Gene table_gene = new org.copakb.server.dao.model.Gene();
-            table_gene.setGene_name(gene.getGeneName().getValue());
-            genes.add(table_gene);
-            System.out.println("gene = " + gene.getGeneName().getValue());
-        }
-        result.setGenes(genes);
-
         // fill in keywords
         String keywords = "";
         for(Keyword k: e.getKeywords()){
@@ -175,17 +168,32 @@ public class ProteinUpdate {
 
         // fill in cross reference, domain, and ensemble id
         String goTerms = "", crossRef = "", ensembl = "";
+        Set<GoTerms> protGoTerms = new HashSet<GoTerms>();
+        Set<String> ensemblIds = new HashSet<String>();
 
         for(uk.ac.ebi.kraken.interfaces.uniprot.DatabaseCrossReference d: e.getDatabaseCrossReferences())  {
             String temp_dName = d.getDatabase().getName();
             // d.getDatabase().toDisplayName();
+            // create go term objects as they are parsed
             if(temp_dName.toUpperCase().substring(0, 2).equals("GO")){
-                goTerms += d.toString()+"|";
+                String fullTerm = d.toString();
+                int goAcc = Integer.parseInt(fullTerm.substring(7, 14));
+                String goTermInfo = fullTerm.substring(15);
+
+                // create completed GoTerms object
+                org.copakb.server.dao.model.GoTerms tempGO = new org.copakb.server.dao.model.GoTerms(goAcc, goTermInfo, null);
+                tempGO.getProteins();
+
+                protGoTerms.add(tempGO);
+                goTerms += d.toString()+"|"; // for printing purposes only
                 continue;
             }
 
             if(temp_dName.toUpperCase().contains("ENSEMBL")){
                 if(d.hasThird()) {
+                    //System.out.println(d.getThird().getValue());
+                    String tempEnsembl = d.getThird().getValue();
+                    ensemblIds.add(tempEnsembl); // add to HashSet to remove duplicates
                     ensembl += d.getThird().getValue() + "\n";
                 }
             }
@@ -194,10 +202,53 @@ public class ProteinUpdate {
             crossRef += d+"\n";
         }
 
-        //Todo insert goTerms, crossRef, ensembl into ProteinCurrent result object
+        result.setGoTerms(protGoTerms);
+
+        // get gene name
+        Set<org.copakb.server.dao.model.Gene> genes = new HashSet<org.copakb.server.dao.model.Gene>();
+        // create gene - ensembl mapping information at the end (after duplicates are removed)
+        for(String id : ensemblIds)
+        {
+            try {
+                String relatedGene = getGeneFromEnsembl(id);
+                org.copakb.server.dao.model.Gene table_gene = new org.copakb.server.dao.model.Gene();
+                table_gene.setGene_name(relatedGene);
+                table_gene.setEnsembl_id(id);
+                table_gene.getDiseases();
+                table_gene.getProteins();
+                table_gene.getHpas();
+                genes.add(table_gene);
+                System.out.println("gene = " + relatedGene + "\trelated ensembl = " + id);
+            }
+            catch (Exception exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+        result.setGenes(genes);
+
+        // Todo: Settle out gene/ensembl mapping
+        // If the gene list given by uniprot always matches the corresponding genes referenced by the list of ensembl id
+        // it would be easier to use the ensembl API (as above) to fill in gene/ensembl for Gene object information
+        // There are separate lists so we have to manually check to see which ensembl ids match
+        // to which gene info because there can be multiple ensembl -> one gene AND multiple ensembl -> multiple genes
+        // The following section is commented out in order to ignore the separate Gene list and just
+        // to go by the ensembl list, WILL BE VERIFIED
+
+        // get gene name
+        /*Set<org.copakb.server.dao.model.Gene> genes = new HashSet<org.copakb.server.dao.model.Gene>();
+        for(Gene gene: e.getGenes()) {
+            org.copakb.server.dao.model.Gene table_gene = new org.copakb.server.dao.model.Gene();
+            table_gene.setGene_name(gene.getGeneName().getValue());
+            genes.add(table_gene);
+            System.out.println("gene = " + gene.getGeneName().getValue());
+        }
+        result.setGenes(genes);*/
+
+        // chromosome
 
         System.out.println("uniprotid = " + uniprotid);
-        System.out.println("sequence = " + result.getSequence());
+        /*System.out.println("sequence = " + result.getSequence());
         System.out.println("weight = " + result.getMolecular_weight());
         System.out.println("name = " + result.getProtein_name());
         System.out.println("features = " + features);
@@ -205,13 +256,70 @@ public class ProteinUpdate {
         System.out.println("noncytoplasmic = " + noncytoplasmic);
         System.out.println("cytoplasmic = " + cytoplasmic);
         System.out.println("transmem = " + transmem);
-        System.out.println("keywords = " + keywords);
-        System.out.println("ensembl = " + ensembl);
-        System.out.println("crossRef = " + crossRef);
-        System.out.println("goTerms = " + goTerms);
+        System.out.println("keywords = " + keywords);*/
+        //System.out.println("ensembl = " + ensembl);
+        //System.out.println("crossRef = " + crossRef);
+        //System.out.println("goTerms = " + goTerms);
 
         return result;
     }
+
+    /**
+     * Code adapted from rest.ensembl.org.
+     * @param ensemblID
+     * @return
+     * @throws Exception
+     */
+    public static String getGeneFromEnsembl(String ensemblID) throws Exception {
+        String server = "http://rest.ensembl.org";
+        String ext = "/lookup/id/" + ensemblID + "?";
+        URL url = new URL(server + ext);
+
+        URLConnection connection = url.openConnection();
+        HttpURLConnection httpConnection = (HttpURLConnection)connection;
+
+        httpConnection.setRequestProperty("Content-Type", "application/json");
+
+
+        InputStream response = connection.getInputStream();
+        int responseCode = httpConnection.getResponseCode();
+
+        if(responseCode != 200) {
+            throw new RuntimeException("Response code was not 200. Detected response was "+responseCode);
+        }
+
+        String output;
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(response, "UTF-8"));
+            StringBuilder builder = new StringBuilder();
+            char[] buffer = new char[8192];
+            int read;
+            while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+                builder.append(buffer, 0, read);
+            }
+            output = builder.toString();
+        }
+        finally {
+            if (reader != null) try {
+                reader.close();
+            } catch (IOException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+        }
+
+        // parse the json information to find just the "display name" which corresponds to gene symbol
+        Pattern pattern = Pattern.compile("\"display_name\":\"(.+?)\"");
+        Matcher matcher = pattern.matcher(output);
+        if(matcher.find())
+        {
+            String geneSymbol = matcher.group(0).substring(16, matcher.group(0).length()-1);
+            return geneSymbol;
+        }
+        return "";
+        //System.out.println(output);
+    }
+
     /*
     public static int calcDistinctPeptides(Connection conn, String protein_id){
 
